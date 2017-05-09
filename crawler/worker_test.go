3da921473dbb56145e35d1448c43e87c587bcc29
Simon Backx <hi@simonbackx.com>
2017-05-09 23:50:41 +0200

    Worker test empty

---
+++ crawler/worker_test.go
@@ -0,0 +1,15 @@
+package crawler
+
+import (
+	"net/url"
+	"testing"
+)
+
+func TestWorker(test *testing.T) {
+
+}
+
+/*
+ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintln(w, "Hello, client")
+    }))*/
